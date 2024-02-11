712b7b2e292cb7f54e6ca2df9b3afde50b50ecfd
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-10 20:36:50 -0800

    commit

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ import currencies from './currencies.js';
 const amountInput = document.getElementById('amount');
 const fromCurrencySelect = document.getElementById('fromCurrency');
 const toCurrencySelect = document.getElementById('toCurrency');
-
+const convertBtn = document.getElementById('convertBtn');
+const resultText = document.getElementById('result');
+const searchInput = document.getElementById('searchInput');
+const addCurrencyBtn = document.getElementById('addCurrencyBtn');
+const customCurrencyInput = document.getElementById('customCurrencyInput');
