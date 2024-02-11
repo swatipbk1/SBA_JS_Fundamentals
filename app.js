0e73d95441dd63d10d0156047f5f5d1509f34b6c
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-10 20:19:26 -0800

    commit

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 import { fetchExchangeRates } from './exchangeRates.js';
 import currencies from './currencies.js';
+
+
+const amountInput = document.getElementById('amount');
+const fromCurrencySelect = document.getElementById('fromCurrency');
+const toCurrencySelect = document.getElementById('toCurrency');
+
