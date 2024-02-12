4f2cf09c8993fbf3c9416847a0cadd0cee4b2858
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-11 20:51:29 -0800

    c

---
+++ exchangeRates.js
@@ -0,0 +1,6 @@
+const API_KEY = '34b7875b-89d4-4ce2-8fa4-42d73033f520'; // Oanda API key
+const API_URL = 'https://api.exchangerate-api.com/v4/latest/';
+
+async function fetchExchangeRates(baseCurrency) {
+  try {
+   
